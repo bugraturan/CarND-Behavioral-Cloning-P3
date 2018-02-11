a3bc0fe14160513ae4b33e9b7c073a97e18e5a7d
bugraturan <buggi1984@gmail.com>
2018-02-11 21:44:27 +0100

    Final commit

--- model.py
+++ model.py
@@ -10,6 +10,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import argparse
 
 #import keras
 from keras.models import Sequential
@@ -29,7 +30,9 @@ from sklearn.utils import shuffle
 ########################### Helper Methods #################################
 
 def gen_hist_plot(angles):
-    # Prints a histogram for "class" distribution overview
+    """
+    Prints a histogram for steering angle distribution overview
+    """
     num_bins = 25
     hist, bins = np.histogram(angles, num_bins)
     width = 0.6 * (bins[1] - bins[0])
@@ -37,15 +40,52 @@ def gen_hist_plot(angles):
     plt.bar(center, hist, align='center', width=width)
     plt.show()
 
+def augment_discriminator(angle, glob_augment_probs, glob_bins):
+    """
+    Decide if augmentation needs to be applied
+    """
+    for i in range(len(glob_bins)):
+        #check in which angle bin the angle is
+        if angle > glob_bins[i] and angle <= glob_bins[i+1]:
+            return (np.random.rand() > glob_augment_probs[i])
+
+def random_augment_operation(image, angle):
+    """
+    Execute random augmentation operation
+    """
+    op = random.choice([1,2,3,4])
+
+    #change to <= for multiple augmentations on same image
+    if op ==1:
+        #add random brightness
+        image = shadow(image)
+    if op ==2:
+        #add random brightness
+        image = brightness(image)
+    if op ==3:
+        #randomly flip
+        image, angle = vflip(image, angle)
+    if op ==4:
+        #randomly translate
+        image, angle = translate(image, angle)
+
+    return image, angle
+
 ########################### Augmentation Methods ###########################
 
 def vflip(image, steering_angle):
+    """
+    Vertically flip image
+    """
     if np.random.rand() < 0.5:
         image = cv2.flip(image, 1)
         steering_angle = steering_angle*(-1.0)
     return image, steering_angle
 
 def translate(image, steering_angle, dx=10, dy=10):
+    """
+    Translates image within defined pixel range
+    """
     shiftx = dx * (np.random.rand() - 0.5)
     shifty = dy * (np.random.rand() - 0.5)
     steering_angle += shiftx * 0.002
@@ -55,30 +95,27 @@ def translate(image, steering_angle, dx=10, dy=10):
     return image, steering_angle
 
 def shadow(image):
-    # (x1, y1) and (x2, y2) forms a line
-    # xm, ym gives all the locations of the image
+    """
+    Apply random shadow
+    """
     x1, y1 = 320 * np.random.rand(), 0
     x2, y2 = 320 * np.random.rand(), 160
     xm, ym = np.mgrid[0:160, 0:320]
 
-    # mathematically speaking, we want to set 1 below the line and zero otherwise
-    # Our coordinate is up side down.  So, the above the line: 
-    # (ym-y1)/(xm-x1) > (y2-y1)/(x2-x1)
-    # as x2 == x1 causes zero-division problem, we'll write it in the below form:
-    # (ym-y1)*(x2-x1) - (y2-y1)*(xm-x1) > 0
     mask = np.zeros_like(image[:, :, 1])
     mask[(ym - y1) * (x2 - x1) - (y2 - y1) * (xm - x1) > 0] = 1
 
-    # choose which side should have shadow and adjust saturation
     cond = mask == np.random.randint(2)
     s_ratio = np.random.uniform(low=0.2, high=0.5)
 
-    # adjust Saturation in HLS(Hue, Light, Saturation)
     hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
     hls[:, :, 1][cond] = hls[:, :, 1][cond] * s_ratio
     return cv2.cvtColor(hls, cv2.COLOR_HLS2RGB)
 
 def brightness(image):
+    """
+    Change brightness of image
+    """
     hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
     ratio = 1.0 + 0.4 * (np.random.rand() - 0.5)
     hsv[:,:,2] =  hsv[:,:,2] * ratio
@@ -87,12 +124,18 @@ def brightness(image):
 ########################### Pipeline Functions #############################
 
 def resize(img):
-    #resize directly within the pipeline
+    """
+    Resize directly within the pipeline
+    """
     return ktf.image.resize_images(img, [66, 200])
 
 def read_driving_logs(driving_logs):
-
-    print("Reading all driving logs...")
+    """
+    Read all driving logs from the given paths
+    """
+    print('-' * 50)
+    print("1. Reading all driving logs...")
+    print('-' * 50)
 
     lines = []
 
@@ -108,7 +151,11 @@ def read_driving_logs(driving_logs):
 
     return lines
 
-def generator(samples, batch_size=128, augment=True):
+def generator(samples, batch_size, augment=False, augmentation_divider=16, use_angle_dep_augmentation=False, glob_augment_probs=None, glob_bins=None):
+    """
+    Data batch generator
+    """
+
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         shuffle(samples)
@@ -126,46 +173,57 @@ def generator(samples, batch_size=128, augment=True):
                 center_image = cv2.imread(current_path)
 
                 #convert to YUV
-                center_image = cv2.cvtColor(center_image, cv2.COLOR_BGR2YUV) #It seems that this is required?
+                center_image = cv2.cvtColor(center_image, cv2.COLOR_BGR2YUV)
                 center_angle = float(batch_sample[3])
 
                 #append original data
                 angles.append(center_angle)
                 images.append(center_image)
 
-            #augment batch
+            #augment batch?
             if augment:
-                #augment one quarter of the batch
-                for i in range(int(batch_size/16)):
+                #how much of the current batch do we want to augment?
+                for i in range(int(batch_size/augmentation_divider)):
 
-                    #add random shadow
-                    index = random.randint(0, len(images)-1)
-                    images[index] = shadow(images[index])
+                    if use_angle_dep_augmentation:
+                        #choose random data from current batch
+                        index = random.randint(0, len(images)-1)
 
-                    #add random brightness
-                    index = random.randint(0, len(images)-1)
-                    images[index] = brightness(images[index])
+                        #do we want to augment this angle?
+                        if augment_discriminator(angles[index], glob_augment_probs, glob_bins):
+                            #apply random augmentation operation
+                            images[index], angles[index] = random_augment_operation(images[index], angles[index])
+                    else:
+                        #add random shadow
+                        index = random.randint(0, len(images)-1)
+                        images[index] = shadow(images[index])
 
-                    #randomly flip
-                    index = random.randint(0, len(images)-1)
-                    images[index], angles[index] = vflip(images[index], angles[index])
+                        #add random brightness
+                        index = random.randint(0, len(images)-1)
+                        images[index] = brightness(images[index])
 
-                    #randomly translate
-                    index = random.randint(0, len(images)-1)
-                    images[index], angles[index] = translate(images[index], angles[index])
+                        #randomly flip
+                        index = random.randint(0, len(images)-1)
+                        images[index], angles[index] = vflip(images[index], angles[index])
+
+                        #randomly translate
+                        index = random.randint(0, len(images)-1)
+                        images[index], angles[index] = translate(images[index], angles[index])
 
             X_train = np.array(images)
             y_train = np.array(angles)
             yield shuffle(X_train, y_train)
 
-def build_model():
+def build_model(keep_prob1, keep_prob2, crop_top, crop_bot):
     
-    print("4. Build model...")
+    print('-' * 50)
+    print("4. Building model...")
+    print('-' * 50)
 
     #input
     model = Sequential()
     model.add(Lambda(lambda x: ((x / 127.5) - 1), input_shape=(160,320,3), output_shape=(160,320,3)))
-    model.add(Cropping2D(cropping=((70,25), (0,0))))
+    model.add(Cropping2D(cropping=((crop_top,crop_bot), (0,0))))
     model.add(Lambda(resize))
 
     #layer1
@@ -186,14 +244,14 @@ def build_model():
     model.add(Dense(100))
 
     #dropout
-    model.add(Dropout(0.5))
+    model.add(Dropout(keep_prob1))
     model.add(Activation('relu'))
 
     #fully connected 2
     model.add(Dense(50))
 
     #dropout
-    model.add(Dropout(0.25))
+    model.add(Dropout(keep_prob2))
     model.add(Activation('relu'))
 
     #fully connected 3
@@ -207,18 +265,20 @@ def build_model():
 
     return model
  
-def train_model(model, train_generator, validation_generator, lr=0.0001, epochs=5):
+def train_model(model, train_generator, validation_generator, train_samples,  validation_samples, lr, epochs, samples_per_epoch_divider):
     
-    print("5. Train model...")
+    print('-' * 50)
+    print("5. Training model...")
+    print('-' * 50)
     
     model.compile(loss="mse", optimizer=Adam(lr=lr))
 
     checkpoint = ModelCheckpoint('model{epoch:02d}.h5')
 
     history_object = model.fit_generator(train_generator,
-        steps_per_epoch= len(train_samples)/5,
+        steps_per_epoch= len(train_samples)/samples_per_epoch_divider,
         validation_data=validation_generator,
-        validation_steps=len(validation_samples)/5,
+        validation_steps=len(validation_samples)/samples_per_epoch_divider,
         epochs=epochs,
         verbose = 1,
         callbacks=[checkpoint])
@@ -229,34 +289,89 @@ def train_model(model, train_generator, validation_generator, lr=0.0001, epochs=
 
 ########################### Main Part #######################################
 
-if __name__ == '__main__':
+def main():
+
+    #argument parser
+    parser = argparse.ArgumentParser(description='Behavioral Cloning')
+    parser.add_argument('-keep1', help='drop out probability', dest='keep_prob1', type=float, default=0.5)
+    parser.add_argument('-keep2', help='drop out probability', dest='keep_prob2', type=float, default=0.25)
+    parser.add_argument('-epochs', help='number of epochs', dest='epochs', type=int,   default=5)
+    parser.add_argument('-s_div', help='samples per epoch divider', dest='samples_per_epoch_divider', type=int, default=5)
+    parser.add_argument('-batch_size', help='batch size', dest='batch_size', type=int, default=128)
+    parser.add_argument('-batch_aug_div', help='batch augmentation divider', dest='augmentation_divider', type=int, default=16)
+    parser.add_argument('-angDep', help='Use angle dep augmentation', dest='use_angle_dep_augmentation', type=bool, default=True)
+    parser.add_argument('-lr', help='learning rate', dest='learning_rate', type=float, default=1.0e-4)
+    parser.add_argument('-c_top', help='crop top', dest='crop_top', type=int, default=70)
+    parser.add_argument('-c_bot', help='crop bottom', dest='crop_bot', type=int, default=25)
+    args = parser.parse_args()
+
+    #print parameters
+    print('-' * 50)
+    print('0. Read all parameters')
+    print('-' * 50)
+    for key, value in vars(args).items():
+        print('{:<30} := {}'.format(key, value))
 
     #read the driving logs of all measurements
     driving_logs=["data_fwd/driving_log.csv",
-                  "data_rev/driving_log.csv",
-                  "data_recov_fwd/driving_log.csv",
-                  "data_recov_rev/driving_log.csv",]
+          "data_rev/driving_log.csv",
+          "data_recov_fwd/driving_log.csv",
+          "data_recov_rev/driving_log.csv",]
 
     lines = read_driving_logs(driving_logs)
 
     #plot histogram of steering angle distribution
+    print('-' * 50)
+    print("2. Plot steering angle distribution...")
+    print('-' * 50)
     angles=[]
     for line in lines:
         angles.append(float(line[3]))
     gen_hist_plot(angles)
 
+    #create list with inverse of the steering angle distribution
+    print('-' * 50)
+    print("3. Define augmentation policy...")
+    print('-' * 50)
+    num_bins = 25
+    hist, glob_bins = np.histogram(angles, num_bins)
+    glob_augment_probs = []
+    target = sum(hist)/num_bins * 0.5
+    for i in range(num_bins):
+        if hist[i] < target:
+            glob_augment_probs.append(1.0)
+        else:
+            glob_augment_probs.append(target/hist[i])
+
+    plt.plot(glob_augment_probs)
+    plt.show()
+    
     #split log lines set
     train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
     #define generator functions
-    train_generator = generator(train_samples, batch_size=128)
-    validation_generator = generator(validation_samples, batch_size=128, augment=False)
+    train_generator = generator(train_samples, 
+        args.batch_size, 
+        augment=True,
+        augmentation_divider=args.augmentation_divider,
+        use_angle_dep_augmentation=args.use_angle_dep_augmentation,
+        glob_augment_probs=glob_augment_probs, 
+        glob_bins=glob_bins)
+
+    validation_generator = generator(validation_samples, args.batch_size)
 
     #build model from NVIDIA paper with dropout
-    model = build_model()
+    model = build_model(args.keep_prob1, args.keep_prob2, args.crop_top, args.crop_bot)
 
     #train the model
-    history_object = train_model(model, train_generator, validation_generator)
+    history_object = train_model(model, 
+        train_generator, 
+        validation_generator,
+        train_samples,
+        validation_samples,
+        args.learning_rate, 
+        args.epochs,
+        args.samples_per_epoch_divider)
 
     #plot the training and validation results
     plt.plot(history_object.history['loss'])
@@ -266,3 +381,6 @@ if __name__ == '__main__':
     plt.xlabel('epoch')
     plt.legend(['training set', 'validation set'], loc='upper right')
     plt.show()
+
+if __name__ == '__main__':
+    main()
